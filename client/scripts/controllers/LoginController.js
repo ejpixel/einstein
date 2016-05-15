02e7afa04556d9ae8b57ee0cf9613d6320138572
Nicolas Busato <nicbusato@gmail.com>
2016-05-14 23:42:54 -0300

    css chamada melhorado

---
+++ client/scripts/controllers/LoginController.js
@@ -0,0 +1,32 @@
+angular.module("Estrutura-Inicial")
+    .controller('LoginController', LoginController);
+
+function LoginController($location, $scope, $ionicPopup) {
+    var vm = this;
+
+    vm.doLogin = function() {
+
+		if(!vm.login || !vm.pass){
+			Meteor.logout();
+			fail($ionicPopup);
+			return;
+		}
+		Meteor.loginWithPassword(vm.login, vm.pass, function(error) {
+			console.log(error);
+
+			if(!error){
+	        	$location.path('/einstein/first-page');
+			}else {
+				fail($ionicPopup);
+			}
+		});
+    }
+
+	function fail($ionicPopup){
+		$ionicPopup.alert({
+			title: 'Usuário ou senha incorretos!',
+			template: 'Por favor tente novamente'
+		});
+	}
+
+}
