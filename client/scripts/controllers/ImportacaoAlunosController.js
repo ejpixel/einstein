d53b435c9ad7cdc7c1d66f68ecdb7005ecd55543
deciomoritz <deciomoritz1@gmail.com>
2016-11-30 03:11:43 -0200

    WIP

---
+++ client/scripts/controllers/ImportacaoAlunosController.js
@@ -0,0 +1,44 @@
+angular.module("Estrutura-Inicial")
+    .controller('ImportacaoAlunosController', ImportacaoAlunosController);
+
+function ImportacaoAlunosController($state, $scope) {
+
+    var vm = this;
+    
+    vm.importarAlunos = function() {
+        var fileElem = document.getElementById("file").files[0];
+
+        if(!fileElem){
+            $ionicPopup.alert({
+               title: 'Arquivo inválido ou inexistente!',
+               template: 'Por favor insira um arquivo com extensão .csv'
+           });
+        }
+
+        var reader = new FileReader();
+        reader.readAsText(fileElem);
+        reader.addEventListener("load", function () {
+            vm.handlePlainText(reader.result);
+        }, false);
+    };
+
+    vm.handlePlainText = function(plainText) {
+        console.log(plainText);
+
+        var lines = plainText.split("\n");
+        lines.splice(0,1);
+        console.log(lines);
+
+        angular.forEach(lines, function(line) {
+            var columns = line.split(",");
+
+            let matricula = columns[0];
+            let nome = columns[1];
+
+            Alunos.insert({
+                matricula: matricula,
+                nome: nome
+            });
+        });
+    };
+};
